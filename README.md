912d16bc3f0a7530cb505a88bfded55ea69f420d
soyandrestrujillo <andymania.1301@gmail.com>
2023-08-09 10:28:59 -0500

    Created project test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # API-SSFF
 This repository is for connect to SSFF SAP and return data in a JSON.
+
+This is the link of the documentation to connect for the API SSFF:
+[OData V2](https://help.sap.com/doc/a7c08a422cc14e1eaaffee83610a981d/2305/en-US/SF_HCM_OData_API_DEV.pdf)
---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000; // Puerto en el que se ejecutará el servidor
+
+// Ruta para hacer una solicitud a la API
+app.get('/api/products', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.escuelajs.co/api/v1/products');
+        res.json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({ error: error.message });
+    }
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
